3ce5125d2a4bc26d7580d6cc1db88802e3d6f2b2
Timothy-moses714 <timoajiji@gmail.com>
2025-06-26 14:22:44 +0100

    update

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+var user = {
+    username: "John Mark",
+    password: "myPass123",
+};
+
+var database = [user];
+
+var newsfeed = [
+  {
+    username: "John Doe",
+    timeline: "Just finished my first coding!"
+  },
+  {
+    username: "Jany smith",
+    timeline: "Loving the new JavaScript!"
+  },
+  {
+    username: "master",
+    timeline: "Debugging is too hard!"
+  }
+];
