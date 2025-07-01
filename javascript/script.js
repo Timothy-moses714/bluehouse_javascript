41ddac5f6c6b38a3632d27ad973a4a9d1530ce48
Timothy-moses714 <timoajiji@gmail.com>
2025-07-01 12:56:46 +0100

    facebook clone

--- javascript/script.js
+++ javascript/script.js
@@ -19,3 +19,43 @@ var newsfeed = [
     timeline: "Debugging is too hard!"
   }
 ];
+
+/* facebook clone*/
+var database = [
+    {
+        username: "salisu",
+        password: "adamu"
+    }
+];
+
+var newsfeed = [
+    {
+        username: "Tboy",
+        timeline: "Javascript is hard"
+    },
+
+
+
+
+    
+     {
+        username: "John",
+        timeline: "hooks and jumps"
+    },
+
+     {
+        username: "Mark",
+        password: "it's geeting interesting"
+    },
+];
+var usernameprompt = prompt("username");
+var passwordprompt = prompt("password");
+function signIn(user, pass){
+    if (user === database.username && pass === database[0].password){
+        console.log(newsfeed);
+
+    } else {
+        alert("wrong details")
+    }
+};
+signIn(usernameprompt, passwordprompt);
