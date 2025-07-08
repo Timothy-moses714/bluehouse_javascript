182f5f37673bb3f48f69a9616b5a557e2db263d0
Timothy-moses714 <timoajiji@gmail.com>
2025-07-08 13:01:51 +0100

    update

---
+++ loops in Javascript/script.js
@@ -0,0 +1,23 @@
+/*write a for loop that print even number
+from */ 
+var number = 2;
+for (var number = 2; number <= 10; number++){
+    if (number % 2 === 0){
+         console.log(number)
+    }
+   
+};
+
+/*write a while lopp that count from 10 to 1 */ 
+var count = 10;
+while (count >= 1){
+    console.log(count)
+    count--;
+};
+
+/* write a do while loop that print a number 5 
+even though the condition is false*/
+var num  = 5;
+do {
+    console.log(num)
+} while (num < 2)
